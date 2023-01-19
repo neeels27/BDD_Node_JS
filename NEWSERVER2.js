e865db12d569b320bec3370e6dbfd583d9a541b0
YannisFouzi <yfouzi.dev@gmail.com>
2023-01-19 16:48:07 +0100

    search & crypto id

--- NEWSERVER2.js
+++ NEWSERVER2.js
@@ -1,9 +1,9 @@
 const http = require('http');
 const url = require('url');
 const fs = require('fs');
+const crypto = require ('crypto');
 
 let data = [];
-let lastId = 0;
 
 try {
   data = JSON.parse(fs.readFileSync('database.json'));
@@ -12,15 +12,26 @@ try {
 }
 
 function findById(id) {
-  return data.find(item => item.id === id);
+    return data.find(item => item.id === id);
 }
 
 function handleRequest(req, res) {
-  const parsedUrl = url.parse(req.url, true);
-  const path = parsedUrl.pathname;
-  const query = parsedUrl.query;
-  const method = req.method;
+    const parsedUrl = url.parse(req.url, true);
+    const path = parsedUrl.pathname;
+    const query = parsedUrl.query;
+    const method = req.method;
 
+    function searchItems(query) {
+      const lowerQuery = query.toLowerCase();
+      return data.filter(item => {
+          for (let key in item) {
+              if (item[key].toString().toLowerCase().includes(lowerQuery)) {
+                  return true;
+              }
+          }
+          return false;
+      });
+  }
   switch (method) {
     case 'GET':
       if (path === '/') {
@@ -30,7 +41,8 @@ function handleRequest(req, res) {
       } else if (path.startsWith('/items/')) {
         // Renvoyer un élément spécifique en fonction de son ID
         const id = parseInt(path.split('/')[2]);
-        const item = findById(id);
+        const lowerId = id.toString().toLowerCase();
+        const item = findById(lowerId);        
         if (item) {
           res.setHeader('Content-Type', 'application/json');
           res.end(JSON.stringify(item));
@@ -38,7 +50,19 @@ function handleRequest(req, res) {
           res.statusCode = 404;
           res.end();
         }
-      } else {
+      } else if(path.startsWith('/search')) {
+        // Rechercher les éléments qui correspondent à la requête
+        const searchQuery = query.q;
+        const searchResults = searchItems(searchQuery);
+        if (searchResults) {
+          res.setHeader('Content-Type', 'application/json');
+          res.end(JSON.stringify(searchResults));
+        } else {
+          res.statusCode = 404;
+          res.end();
+        }
+      }
+      else {
         // Renvoyer une erreur si l'URL est incorrecte
         res.statusCode = 404;
         res.end();
@@ -47,18 +71,20 @@ function handleRequest(req, res) {
       case 'POST':
         let body = '';
         req.on('data', chunk => {
-          body += chunk;
+            body += chunk;
         });
+
         req.on('end', () => {
+            let lastId = 0;
             const item = JSON.parse(body);
-            lastId = 0;
-            item.id = ++lastId;
-            fs.writeFileSync('database'+ Date.now() +'.json', JSON.stringify(item));
+            item.id = crypto.randomBytes(16).toString('hex');
+            data.push(item);
+            fs.writeFileSync('database'+ Date.now() +'.json', JSON.stringify(data));
             res.setHeader('Content-Type', 'application/json');
             res.end(JSON.stringify(item));
         });
         break;
-    
+
         case 'PUT':
           // Modifier un élément existant dans la liste
           const id = parseInt(path.split('/')[2]);
@@ -95,10 +121,12 @@ function handleRequest(req, res) {
           break;
     }
   }
-  
+
   const server = http.createServer(handleRequest);
-  
+
   server.listen(3000, () => {
     console.log('Server listening on port 3000');
   });
-  
+
+  //uniquement boolean, int, string
+//mettre des objets au lieu du tableau pour avoir l'idée du fichier
